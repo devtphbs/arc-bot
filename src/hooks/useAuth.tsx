27959fb6766329cc1a49cabfc6d2e81c1bb405f1
Hilding Westerberg Sundh <hildingws2013@gmail.com>
2026-03-26 16:00:49 +0100

    auth

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -57,7 +57,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: window.location.origin,
+        redirectTo: 'https://arc-bot-free.vercel.app',
       },
     });
     if (error) {
